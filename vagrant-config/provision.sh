0e4f00dffa1a2128b82b22dc40483dcf3c09e717
Chris Harrell <john.c.harrell@gmail.com>
2015-05-06 19:35:08 -0500

    Utilize a more up-to-date PPA for Node 0.12.2

--- vagrant-config/provision.sh
+++ vagrant-config/provision.sh
@@ -2,7 +2,8 @@
 
 apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv 7F0CEB10
 echo "deb http://repo.mongodb.org/apt/ubuntu "$(lsb_release -sc)"/mongodb-org/3.0 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-3.0.list
-apt-add-repository -y ppa:chris-lea/node.js
+curl -sL https://deb.nodesource.com/setup_0.12 | sudo bash -
+
 
 apt-get update
 apt-get install -y mongodb-org nginx nodejs git linux-headers-$(uname -r) build-essential
